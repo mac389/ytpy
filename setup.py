7a207140ddd67726d176103b72cf931ea6ef14a2
Michael Chary <mac389@gmail.com>
2013-02-26 15:11:17 -0500

    Began setup.py
    Will finish once package structure has stabilized

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from disutils.core import setup
+
+setup(name="ytpy",
+	  version=1.0
+	  description="Python Wrapper for YouTube API"
+	  author="Michael Chary"
+	  author_email="michael.chary@mssm.edu"
+	  url="https://github.com/mac389/ytpy"
+	  py_modules=[],
+	  )
+	 
