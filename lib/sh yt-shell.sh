1d838a52a5c473f035fbc4e2c140389e141734ef
Michael Chary <mac389@gmail.com>
2013-03-20 15:37:17 -0400

    Reorganized lib

--- lib/sh yt-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cat drugs | while read line
-do
-	echo $line
-	python ytpy.py $line
-	curl http://localhost:5984/_replicate -H 'Content-Type: application/json' -d '{ "source": "yt-cocaine", "target": "https://acharya:marathon221$@acharya.cloudant.com/yt-cocaine" }'
-done
